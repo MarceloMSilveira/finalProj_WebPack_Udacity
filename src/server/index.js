a6a136fa2792c75820e65550de3bead318fb287d
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2023-05-04 18:21:09 -0300

    config post route

--- src/server/index.js
+++ src/server/index.js
@@ -20,35 +20,27 @@ app.use(express.static('dist'))
 console.log(__dirname)
 
 //REQUEST TO API
-const formdata = new FormData();
-formdata.append("key", process.env.API_KEY);
-formdata.append("txt", "THE SKY IS BLUE");
-formdata.append("lang", "es");  // 2-letter code, like en es fr ...
 
-const requestOptions = {
-  method: 'POST',
-  body: formdata,
-  redirect: 'follow'
-};
+function askMeaningCloudAPI (userText) {
+    const formdata = new FormData();
+    formdata.append("key", process.env.API_KEY);
+    formdata.append("txt", userText);
+    formdata.append("lang", "es");  // 2-letter code, like en es fr ...
 
-/* ORIGINAL FROM API SITE:
-const response = fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
-  .then(response => ({
-    status: response.status, 
-    body: response.json()
-  }))
-  .then(({ status, body }) => console.log(status, body))
-  .catch(error => console.log('error', error));
-*/
+    const requestOptions = {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow'
+    };
 
-//My fetch:
-const response = fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
-  .then(response => response.json())
-  .then(response => 
-        console.log(response.subjectivity)
-    )
-  .catch(error => console.log('error', error));
+    //My fetch:
+    const response = fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
+      .then(response => response.json())
+      .then(response => response.subjectivity)
+      .catch(error => console.log('error', error));
 
+    return response.subjectivity;
+}
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
@@ -63,3 +55,22 @@ app.listen(8051, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+//post route to recieve text to be analized
+app.post('/dataPost', (req,res)=> {
+  let userText = req.body;
+  let response = askMeaningCloudAPI(userText);
+  res.send = response;
+  
+})
+
+
+/* ORIGINAL FROM API SITE:
+const response = fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
+  .then(response => ({
+    status: response.status, 
+    body: response.json()
+  }))
+  .then(({ status, body }) => console.log(status, body))
+  .catch(error => console.log('error', error));
+*/
