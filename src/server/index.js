5eea54e124f98885c8c299b5d369970256646be8
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2023-05-04 16:24:39 -0300

    make first test fetch

--- src/server/index.js
+++ src/server/index.js
@@ -16,9 +16,9 @@ const aplication_key = process.env.API_KEY;
 
 //REQUEST TO API
 const formdata = new FormData();
-formdata.append("key", "20489377c5952a89f770375f1b73862b");
-formdata.append("txt", "YOUR TEXT HERE");
-formdata.append("lang", "TEXT LANGUAGE HERE");  // 2-letter code, like en es fr ...
+formdata.append("key", process.env.API_KEY);
+formdata.append("txt", "THE SKY IS BLUE");
+formdata.append("lang", "es");  // 2-letter code, like en es fr ...
 
 const requestOptions = {
   method: 'POST',
@@ -26,12 +26,22 @@ const requestOptions = {
   redirect: 'follow'
 };
 
+/* ORIGINAL FROM API SITE:
 const response = fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
   .then(response => ({
     status: response.status, 
     body: response.json()
   }))
   .then(({ status, body }) => console.log(status, body))
+  .catch(error => console.log('error', error));
+*/
+
+//My fetch:
+const response = fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
+  .then(response => response.json())
+  .then(response => 
+        console.log(response.body)
+    )
   .catch(error => console.log('error', error));
 
 
