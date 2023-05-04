4e7bd01d2cac2cbff5aa8790b36fa00db79ec8ad
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2023-05-04 13:55:53 -0300

    correct cors require and onblur function

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 var path = require('path')
 const express = require('express')
-const cors = require(cors)
+const cors = require('cors')
 const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
