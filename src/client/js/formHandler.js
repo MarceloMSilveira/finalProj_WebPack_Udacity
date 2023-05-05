4ee8e4c554adcf2def45d50f2e11e2dd532a0e03
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2023-05-05 17:23:33 -0300

    setup posData function

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -5,19 +5,33 @@ function handleSubmit(event) {
     event.preventDefault()
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    fetch('http://localhost:8051/dataPost',{
-        method: 'POST', 
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-       // Body data type must match "Content-Type" header        
-        body: JSON.stringify({
-            userResp:formText}), 
-    })
-    .then(res => res.json())
-    .then(data => document.getElementById('results').innerHTML = data.APIresponse)
-    .catch(error=> console.log("error", error))
+    const url = 'http://localhost:8051/dataPost'
+    postData(url,{userResp:formText})
 }
 
+const postData = async ( url = '', data = {})=>{
+    console.log(data);
+    const response = await fetch(url, {
+    method: 'POST', 
+    credentials: 'same-origin',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+   // Body data type must match "Content-Type" header        
+    body: JSON.stringify(data), 
+  });
+  
+    try {
+      const newData = await response.json();
+      console.log(newData.APIResp);
+      return newData;
+    }catch(error) {
+    console.log("error", error);
+    }
+  };
+
+
 export { handleSubmit }
+
+
+//.then(data => document.getElementById('results').innerHTML = data.APIresponse)
