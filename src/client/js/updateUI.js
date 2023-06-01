f47e8ba38dea8791baf9d5d0c9ff236587412bec
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2023-06-01 11:39:28 -0300

    change update return to newData

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -5,7 +5,7 @@ async function upDateUI() {
         console.log(`inside upDateUI: ${newData.agreement}`)
         document.getElementById('results').innerHTML = `Text extract:: ${newData.sentence_list[0].text}`
         document.getElementById('subjectivity').innerHTML = `Subjectivity: ${newData.subjectivity}`
-        return response
+        return newData
     } catch (error){
         console.log("Error: ", error);
     }
