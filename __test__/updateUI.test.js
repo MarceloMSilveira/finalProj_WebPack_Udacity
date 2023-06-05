455b86411fe2b07bbea6a68a079d27860a0a8b94
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2023-06-05 11:35:57 -0300

    update passed but postData fail

--- __test__/updateUI.test.js
+++ __test__/updateUI.test.js
@@ -1,9 +1,35 @@
 import { upDateUI } from "../src/client";
 
-describe("Testing the asyncFunction functionality", () => {
-    test("Testing the postData() function", () => {
-        return upDateUI().then((result) => {
-            expect(result.subjectivity).toBe('OBJECTIVE');
-        });
-    });
-});
+// Mock para simular a resposta da solicitação
+const mockResponse = {
+    agreement: 'AGREEMENT',
+    sentence_list: [{ text: 'I LOVE YOU' }],
+    subjectivity: 'OBJECTIVE',
+  };
+  
+  // Mock da função fetch
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(mockResponse),
+    })
+  );
+  
+  // Exemplo de teste
+  test('atualiza o DOM corretamente', async () => {
+    // Preparação do DOM simulado
+    document.body.innerHTML = `
+      <div id="results"></div>
+      <div id="subjectivity"></div>
+    `;
+  
+    // Chama a função que será testada
+    await upDateUI();
+  
+    // Verifica se os elementos do DOM foram atualizados corretamente
+    expect(document.getElementById('results').innerHTML).toBe(
+      'Text extract:: I LOVE YOU'
+    );
+    expect(document.getElementById('subjectivity').innerHTML).toBe(
+      'Subjectivity: OBJECTIVE'
+    );
+  });
